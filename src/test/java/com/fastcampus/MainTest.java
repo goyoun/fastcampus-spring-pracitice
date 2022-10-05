1985f4e39848c7abe3985cad7ace6155bb435964
go <monologuess1548@gmail.com>
2022-10-05 17:01:21 +0900

    initialize java project
    지난시간꺼

---
+++ src/test/java/com/fastcampus/MainTest.java
@@ -0,0 +1,20 @@
+package com.fastcampus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //given
+        String [] args = {"3" , "1", "2"};
+
+        //when
+        Main.main(args);
+
+        //then
+    }
+
+}
